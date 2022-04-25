718d6679532194aed5131a357ae39e64e6ff7b13
Piotr Chowaniec <piotr_chowaniec@epam.com>
2022-04-25 11:57:24 +0200

    First changes

---
+++ src/main/java/com/epam/q3ap/jenkinsworkshops/NewClass.java
@@ -0,0 +1,11 @@
+package com.epam.q3ap.jenkinsworkshops;
+
+/**
+ * @author Piotr Chowaniec
+ */
+class NewClass {
+
+    void newMethod() {
+        System.out.println("Hello from newMethod()!");
+    }
+}
